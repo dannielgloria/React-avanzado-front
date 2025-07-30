89a441ab3a9e355172f1ba3cb4f4edc24370f6b5
dannielgloria <danniel.gloria@gmail.com>
2025-07-29 19:26:56 -0600

    feat: Se envuelve el app en main y se unifica la app para solo utilizar postlist como mi todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import './App.css'
 import PostList from './pages/PostList'
-import Login from './pages/Login'
-import Register from './pages/Register'
-import CreatePost from './pages/CreatePost'
 
 function App() {
 
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<PostList />}></Route>
-        <Route path='/login' element={<Login />}></Route>
-        <Route path='/register' element={<Register />}></Route>
-        <Route path='/create' element={<CreatePost />}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+  return <PostList />
 }
 
 export default App
